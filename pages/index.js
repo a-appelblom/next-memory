ada87f22885c3f3deca250bea274d3910a59fb8f
a-appelblom <a.appelblom@gmail.com>
2021-10-26 21:28:52 +0200

    a memory game

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,82 @@
-import Head from 'next/head'
+import { useState, useEffect } from "react";
+import Card from "../components/Card";
 
-export default function Home() {
+const initialCard = { code: "" };
+export default function Home({ cards }) {
+  const [first, setFirst] = useState(initialCard);
+  const [second, setSecond] = useState(initialCard);
+  const [matched, setMatched] = useState(false);
+
+  useEffect(() => {
+    if (matched) {
+      setFirst(initialCard);
+      setSecond(initialCard);
+      setMatched(false);
+    }
+  }, [matched]);
+
+  const getColor = (card) => {
+    if (card.suit === "HEARTS" || card.suit === "DIAMONDS") return "red";
+    return "black";
+  };
+
+  function setCards(card) {
+    if (!first.code) setFirst(card);
+    else if (!second.code && card.code !== first.code) setSecond(card);
+    else if (
+      first.value === second.value &&
+      getColor(first) === getColor(second)
+    ) {
+      setMatched(true);
+    } else {
+      setFirst(initialCard);
+      setSecond(initialCard);
+    }
+  }
+
+  function getSelected(card) {
+    if (card.code === first.code || card.code === second.code) return true;
+    return false;
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+    <div className="grid grid-cols-12 gap-4 p-8 min-h-screen max-w-7xl mx-auto">
+      {cards &&
+        cards.map((card) => {
+          return (
+            <div
+              onClick={() => {
+                setCards(card);
+              }}
+            >
+              <Card
+                key={card.code}
+                image={card.image}
+                selected={getSelected(card)}
+                hasMatched={matched}
+              />
+            </div>
+          );
+        })}
     </div>
-  )
+  );
+}
+
+async function getData(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+}
+
+export async function getStaticProps() {
+  const deck = await getData(
+    "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
+  );
+  const cards = await getData(
+    `https://deckofcardsapi.com/api/deck/${deck.deck_id}/draw/?count=52`
+  );
+  const newCards = cards.cards.map((card) => {
+    return { ...card, matched: false };
+  });
+  return { props: { cards: newCards } };
 }
